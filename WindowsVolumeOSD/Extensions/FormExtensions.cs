bb03b5642f18ee94103de517be93598ec8e7de9a
Greg Flynn <gregflynn42@gmail.com>
2014-06-03 22:11:34 -0400

    added a little code documentation

--- WindowsVolumeOSD/Extensions/FormExtensions.cs
+++ WindowsVolumeOSD/Extensions/FormExtensions.cs
@@ -4,6 +4,12 @@ namespace WindowsVolumeOSD.Extensions
 {
     public static class FormExtensions
     {
+        /// <summary>
+        /// Extension to simplify the InvokeRequired/Invoke pattern
+        /// http://stackoverflow.com/questions/2367718/automating-the-invokerequired-code-pattern
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="action"></param>
         public static void InvokeIfRequired(this Control control, MethodInvoker action)
         {
             if (control.InvokeRequired)
--- WindowsVolumeOSD/Visual/IVolumeDisplay.cs
+++ WindowsVolumeOSD/Visual/IVolumeDisplay.cs
@@ -2,6 +2,12 @@
 {
     interface IVolumeDisplay
     {
+        /// <summary>
+        /// Tells the IVolumeDisplay derivative to update its visual display to the given volume level
+        /// and muted state
+        /// </summary>
+        /// <param name="volumePct">Volume percentage in decimal format, [0,1] such that 0.5 = 50%</param>
+        /// <param name="muted">true if the volume has been muted, false otherwise</param>
         void UpdateVisuals(float volumePct, bool muted);
     }
 }
